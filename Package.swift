c1a547ba684021d76e7ff3ac28ce2513abf7c2b7
ihorbenko <ihorbenko@plexteq.com>
2024-12-20 14:30:12 +0200

    Updated deps

--- Package.swift
+++ Package.swift
@@ -4,32 +4,36 @@ import PackageDescription
 let package = Package(
     name: "ottu_checkout_sdk",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v15)  // Вкажіть мінімальну версію iOS
     ],
     products: [
+        // Додаємо основну бібліотеку з правильним ім'ям для імпорту
         .library(
-            name: "ottu_checkout_sdk",
-            targets: ["ottu_checkout_sdk_wrapper"]
-        )
+            name: "ottu_checkout_sdk",  // Це ім'я, яке ви хочете використовувати при імпорті
+            targets: ["ottu_checkout_sdk"]  // Таргет для фреймворка
+        ),
     ],
     dependencies: [
+        // Додавання залежностей
+        .package(url: "https://github.com/ihorbenko/dummy-ios-framework", from: "1.0.0"),
         .package(url: "https://github.com/getsentry/sentry-cocoa", from: "8.33.0"),
         .package(url: "https://github.com/SVGKit/SVGKit", from: "3.0.0")
     ],
     targets: [
-        // Обгортка, яка зв'язує залежності та binaryTarget
+        // Ваш бінарний таргет (з правильним шляхом до xcframework)
+        .binaryTarget(
+            name: "ottu_checkout_sdk",  // Ім'я фреймворка для імпорту
+            path: "Sources/ottu_checkout_sdk.xcframework"  // Шлях до xcframework
+        ),
+        
+        // Таргет для обгортки, щоб додавати залежності (SVGKit, Sentry)
         .target(
             name: "ottu_checkout_sdk_wrapper",
             dependencies: [
-                "SVGKit",
-                .product(name: "Sentry", package: "sentry-cocoa"),
-                "ottu_checkout_sdk"
-            ],
-	    path: "Sources/ottu_checkout_sdk_wrapper"
+                "SVGKit",  // Додано залежність SVGKit
+                .product(name: "Sentry", package: "sentry-cocoa"),  // Додано залежність Sentry
+                "ottu_checkout_sdk"  // Підключаємо бінарний фреймворк
+            ]
         ),
-        .binaryTarget(
-            name: "ottu_checkout_sdk",
-            path: "Sources/ottu_checkout_sdk.xcframework"
-        )
     ]
 )
