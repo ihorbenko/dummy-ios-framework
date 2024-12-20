dc76da32360a7a69dbc950ea1124489199eeefcb
ihorbenko <ihorbenko@plexteq.com>
2024-12-20 14:53:02 +0200

    Updated deps

--- Package.swift
+++ Package.swift
@@ -4,31 +4,29 @@ import PackageDescription
 let package = Package(
     name: "ottu_checkout_sdk",
     platforms: [
-        .iOS(.v15)  // Вкажіть мінімальну версію iOS
+        .iOS(.v15)  
     ],
     products: [
-        // Основний продукт, який буде імпортуватися як ottu_checkout_sdk
         .library(
-            name: "ottu_checkout_sdk",  // Це ім'я, яке ви хочете використовувати при імпорті
-            targets: ["ottu_checkout_sdk"]  // Таргет для вашого основного фреймворка
+            name: "ottu_checkout_sdk",  
+            targets: ["ottu_checkout_sdk"]
         ),
     ],
     dependencies: [
-        // Залежності (SVGKit та Sentry)
         .package(url: "https://github.com/getsentry/sentry-cocoa", from: "8.33.0"),
         .package(url: "https://github.com/SVGKit/SVGKit", from: "3.0.0")
     ],
     targets: [
-        // Таргет для основного фреймворка з усіма залежностями
+        .binaryTarget(
+            name: "ottu_checkout_sdk_bin",  
+            path: "Sources/ottu_checkout_sdk.xcframework" 
+        ),
         .target(
-            name: "ottu_checkout_sdk",  // Це ім'я фреймворка для імпорту
+            name: "ottu_checkout_sdk",
             dependencies: [
-                "SVGKit",  // Залежність SVGKit
-                .product(name: "Sentry", package: "sentry-cocoa"),  // Залежність Sentry
-                .binaryTarget(
-                    name: "ottu_checkout_sdk_binary",  // Бінарний таргет
-                    path: "Sources/ottu_checkout_sdk.xcframework"  // Шлях до xcframework
-                )  // Ваш бінарний фреймворк
+                "SVGKit",  
+                .product(name: "Sentry", package: "sentry-cocoa"),  
+                "ottu_checkout_sdk_bin" 
             ]
         ),
     ]
