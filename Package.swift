73d67966afbb730a20fc0faec25bcffcf2cd278c
ihorbenko <ihorbenko@plexteq.com>
2024-12-20 14:35:43 +0200

    Updated deps

--- Package.swift
+++ Package.swift
@@ -7,32 +7,31 @@ let package = Package(
         .iOS(.v15)  // Вкажіть мінімальну версію iOS
     ],
     products: [
-        // Додаємо основну бібліотеку з правильним ім'ям для імпорту
+        // Основний продукт, який буде імпортуватися як ottu_checkout_sdk
         .library(
             name: "ottu_checkout_sdk",  // Це ім'я, яке ви хочете використовувати при імпорті
-            targets: ["ottu_checkout_sdk"]  // Таргет для фреймворка
+            targets: ["ottu_checkout_sdk"]  // Таргет для вашого основного фреймворка
         ),
     ],
     dependencies: [
-        // Додавання залежностей
-        .package(url: "https://github.com/ihorbenko/dummy-ios-framework", from: "1.0.0"),
+        // Залежності (SVGKit та Sentry)
         .package(url: "https://github.com/getsentry/sentry-cocoa", from: "8.33.0"),
         .package(url: "https://github.com/SVGKit/SVGKit", from: "3.0.0")
     ],
     targets: [
-        // Ваш бінарний таргет (з правильним шляхом до xcframework)
+        // Ваш бінарний таргет
         .binaryTarget(
             name: "ottu_checkout_sdk",  // Ім'я фреймворка для імпорту
             path: "Sources/ottu_checkout_sdk.xcframework"  // Шлях до xcframework
         ),
         
-        // Таргет для обгортки, щоб додавати залежності (SVGKit, Sentry)
+        // Таргет для основного фреймворка, де будуть підключатися залежності
         .target(
-            name: "ottu_checkout_sdk_wrapper",
+            name: "ottu_checkout_sdk",
             dependencies: [
-                "SVGKit",  // Додано залежність SVGKit
-                .product(name: "Sentry", package: "sentry-cocoa"),  // Додано залежність Sentry
-                "ottu_checkout_sdk"  // Підключаємо бінарний фреймворк
+                "SVGKit",  // Залежність SVGKit
+                .product(name: "Sentry", package: "sentry-cocoa"),  // Залежність Sentry
+                "ottu_checkout_sdk"  // Ваш бінарний фреймворк
             ]
         ),
     ]
