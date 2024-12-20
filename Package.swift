b36a9b8b852b29b586962d761104688f72bffdd1
ihorbenko <ihorbenko@plexteq.com>
2024-12-20 13:10:29 +0200

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -1,41 +1,35 @@
 // swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "ottu_checkout_sdk",
     platforms: [
-        .iOS(.v11) // Specify the minimum iOS version; adjust as necessary
+        .iOS(.v11)
     ],
     products: [
         .library(
             name: "ottu_checkout_sdk",
-            targets: ["ottu_checkout_sdk"]
+            targets: ["ottu_checkout_sdk_wrapper"]
         )
     ],
     dependencies: [
-        // Sentry dependency
         .package(url: "https://github.com/getsentry/sentry-cocoa", from: "8.33.0"),
-        // SVGKit dependency
         .package(url: "https://github.com/SVGKit/SVGKit", from: "3.0.0")
     ],
     targets: [
-        // Declare the binary target
+        // Обгортка, яка зв'язує залежності та binaryTarget
+        .target(
+            name: "ottu_checkout_sdk_wrapper",
+            dependencies: [
+                "SVGKit",
+                .product(name: "Sentry", package: "sentry-cocoa"),
+                "ottu_checkout_sdk"
+            ],
+            path: "Sources/Wrapper" // Створіть цю папку
+        ),
         .binaryTarget(
             name: "ottu_checkout_sdk",
             path: "Sources/ottu_checkout_sdk.xcframework"
-        ),
-        // Example target that might depend on Sentry and SVGKit
-        // Add more targets if required in your project
-        .target(
-            name: "ExampleTarget",
-            dependencies: [
-                .product(name: "Sentry", package: "sentry-cocoa"),
-                "SVGKit"
-            ],
-            path: "Sources/ExampleTarget" // Replace with your target's source path
         )
     ]
 )
-
