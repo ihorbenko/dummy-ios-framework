85d47bdfffc62594aad318550fb1b9ee7a1bba79
Oleksandr Pylypenko <opilipenko@gmail.com>
2024-12-23 12:50:25 +0200

    Update SPM

--- Package.swift
+++ Package.swift
@@ -1,39 +1,18 @@
-// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
     name: "ottu_checkout_sdk",
-    platforms: [
-        .iOS(.v13)
-    ],
+  
     products: [
-        .library(
-            name: "ottu_checkout_sdk",
-            targets: ["ottu_checkout_sdk"]
-        )
+            .library(name: "ottu_checkout_sdk", targets: ["ottu_checkout_sdk"])
     ],
+
     dependencies: [
-        .package(url: "https://github.com/getsentry/sentry-cocoa", from: "8.33.0"),
-        .package(url: "https://github.com/SVGKit/SVGKit", branch: "3.x")
+            .package(url: "https://github.com/getsentry/sentry-cocoa", from: "8.33.0"),
+            .package(url: "https://github.com/SVGKit/SVGKit", branch: "3.x")
     ],
+    
     targets: [
-        .binaryTarget(
-            name: "ottu_checkout_sdk_binary",
-            path: "./Sources/ottu_checkout_sdk.xcframework"
-        ),
-        .target(
-            name: "ottu_checkout_sdk",
-            dependencies: [
-                "ottu_checkout_sdk_binary",
-                .product(name: "Sentry", package: "sentry-cocoa"),
-                "SVGKit"
-            ],
-            path: "./Sources",
-            exclude: ["ottu_checkout_sdk.xcframework"], 
-            linkerSettings: [
-                .linkedFramework("Sentry"),
-                .linkedFramework("SVGKit")
-            ]
-        )
+             .binaryTarget(name: "ottu_checkout_sdk", path: "Sources/ottu_checkout_sdk.xcframework")
     ]
 )
