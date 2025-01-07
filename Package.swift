1acad4da7e5b0e1954b8d4b5d83c0ac260bd5340
Oleksandr Pylypenko <opilipenko@gmail.com>
2025-01-07 14:25:53 +0200

    update

--- Package.swift
+++ Package.swift
@@ -1,35 +1,32 @@
-// swift-tools-version:5.7
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-    name: "ottu_checkout_sdk",
+    name: "dummy-ios-framework",
     platforms: [
-        .iOS(.v13)
-    ],
+          .iOS(.v13)
+      ],
     products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "ottu_checkout_sdk",
-            targets: ["ottu_checkout_sdk", "ottu_checkout_sdk_dep"]
-        )
+            name: "dummy-ios-framework",
+            targets: ["dummy-ios-framework"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/getsentry/sentry-cocoa", from: "8.33.0"),
-        .package(url: "https://github.com/SVGKit/SVGKit", from: "3.0.0")
-    ],
+           .package(url: "https://github.com/username/SWGKit.git", from: "3.0.0"),
+       ],
     targets: [
         .binaryTarget(
-            name: "ottu_checkout_sdk",
-            path: "./Sources/ottu_checkout_sdk.xcframework"
-        ),
+              name: "ottu_checkout_sdk",
+              path: "./path/to/ottu_checkout_sdk.xcframework"),
         .target(
-            name: "ottu_checkout_sdk_dep",
+            name: "dummy-ios-framework",
             dependencies: [
-                         "SVGKit",
-                         .product(name: "Sentry", package: "sentry-cocoa")
-                     ],
-            linkerSettings: [
-                           .linkedLibrary("SVGKit")
+                           .product(name: "SWGKit", package: "SWGKit")
                        ]
-        )
+        ),
+
     ]
 )
