651f2f0fa4e8979bbe8608635dc10df7873d0090
Oleksandr Pylypenko <opilipenko@gmail.com>
2025-01-07 12:11:24 +0200

    Update PS

--- Package.swift
+++ Package.swift
@@ -1,3 +1,4 @@
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
@@ -5,12 +6,27 @@ let package = Package(
     platforms: [
         .iOS(.v13)
     ],
-
     products: [
-            .library(name: "ottu_checkout_sdk", targets: ["ottu_checkout_sdk"])
+        .library(
+            name: "ottu_checkout_sdk",
+            targets: ["ottu_checkout_sdk", "ottu_checkout_sdk_binary"]
+        )
     ],
-    
+    dependencies: [
+        .package(url: "https://github.com/getsentry/sentry-cocoa", from: "8.33.0"),
+        .package(url: "https://github.com/SVGKit/SVGKit", branch: "3.x"),
+        .package(
+            name: "ottu_checkout_sdk_binary",
+            path: "./Sources/ottu_checkout_sdk.xcframework"
+        )
+    ],
     targets: [
-             .binaryTarget(name: "ottu_checkout_sdk", path: "Sources/ottu_checkout_sdk.xcframework")
+        .target(
+            name: "ottu_checkout_sdk",
+            dependencies: [
+                         "SVGKit",
+                         .product(name: "Sentry", package: "sentry-cocoa")
+                     ],
+        )
     ]
 )
